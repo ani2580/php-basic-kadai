6cc793d2e8ba111b3a572f7ff9fe1cda8d92ca8e
ani2580 <ani2580hiro@gmail.com>
2026-03-15 18:01:35 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -1,41 +1,38 @@
 <!DOCTYPE html>
 <html lang="ja">
-  <head>
-    <meta cherset="utf-8">
+
+<head>
+    <meta charset="UTF-8">
     <title>PHP基礎編</title>
-  </head>
+</head>
 
-  <body>
+<body>
     <p>
-    <?php
+        <?php
+        function sort_2way($array, $order) {
 
-    function sort_2way($array, $order) {
-        // この関数内で定義された変数$numsは関数の外から参照できない
-        $nums = [15, 4, 18, 23, 10];
-
-        if($order) {
-            echo '昇順にソートします。<br>';
-
-            // このsort関数に与える配列は1つにする。
-            sort($nums, $array);
-            foreach ($nums as $num) {
-                echo $num . '<br>';
+            if ($order === true) {
+                sort($array);
+                echo '昇順にソートします。<br>';
+            } else {
+                rsort($array);
+                echo '降順にソートします。<br>';
             }
-        } else {
-            echo '降順にソートします。<br>';
-            // このrsort関数に与える配列は1つにする。
-            rsort($nums, $array);
-            foreach ($nums as $num) {
+
+            foreach ($array as $num) {
                 echo $num . '<br>';
             }
         }
-    }
 
-    // 関数の外から$numは参照することはできない
-    sort_2way($nums, TRUE);
-    sort_2way($nums, FALSE);
+        $nums = [15, 4, 18, 23, 10];
 
-    ?>
+        sort_2way($nums, true);
+        echo "<br>";
+
+        sort_2way($nums, false);
+        echo "<br>";
+        ?>
     </p>
-  </body>
+</body>
+
 </html>
